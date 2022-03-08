120748b8dbca0a002443f27b80bf1c2a1180716c
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2022-03-08 16:27:21 +0500

    [bug]: loader spinning on the generated QR code #14 - ensured that QRCode appers once CID is received. - used JQuery's .off() so that one listener is attached. - changed DOM selector to download generated QRCode. - refactored code a bit to prevent code duplication.

--- src/js/qrcode.js
+++ src/js/qrcode.js
@@ -17,31 +17,38 @@ var QR_CODE = new QRCode("qrcode", {
 const token = process.env.API_TOKEN;
 const client = new Web3Storage({ token });
 
-// Generate decentralized QR code from file
-$("#fileUpload").on("change", async function () {
+function hideLoader(callback) {
+  $(".loader").hide(function () {
+    $("section.flex-center > svg").css("aria-disabled", false);
+    if (callback && typeof callback === "function") callback();
+  });
+}
+
+function showLoader() {
   $(".loader").show(900);
-  var files = fileUpload.files;
-  var name = files[0].name;
-  var cid = await client.put(files);
-  let ipfsLink = `https://dweb.link/ipfs/${cid}/${name}/`;
+  $("section.flex-center > svg").css("aria-disabled", true);
+  $("#qrcode img").css("display", "none");
+  $(".output-label + div").text("");
+}
+
+function uploadCallback(cid, ipfsLink) {
   // IPFS hash (CID)
   document.getElementById("cid").textContent = cid;
   // IPFS Infura link
   document.getElementById("link").textContent = ipfsLink;
-  $("#svg-link").on("click", function () {
+  $("#svg-link").off().on("click", function () {
     window.open(ipfsLink, "_blank").focus();
   });
   // Copy CID to clipboard when copy button is clicked
-  $("#svg-cid").on("click", function () {
+  $("#svg-cid").off().on("click", function () {
     navigator.clipboard.writeText(cid);
   });
   // Generate QR code
   QR_CODE.makeCode(ipfsLink);
-  $(".loader").hide();
   // Code to download qrcode
-  $("#svg-download").on("click", function () {
+  $("#svg-download").off().on("click", function () {
     // gets the base64 source of the qr code image
-    var qrCodeSrc = document.getElementById("qr-code-image").src;
+    var qrCodeSrc = document.querySelector("#qrcode img").src;
     var a = document.createElement("a");
     // an invisible a tag is given that href.
     a.href = qrCodeSrc;
@@ -51,43 +58,30 @@ $("#fileUpload").on("change", async function () {
     // the a tag is clicked, triggering the download
     a.click();
     document.body.removeChild(a);
+  });
+}
+
+// Generate decentralized QR code from file
+$("#fileUpload").on("change", async function () {
+  showLoader();
+  var files = fileUpload.files;
+  var name = files[0].name;
+  var cid = await client.put(files);
+  let ipfsLink = `https://dweb.link/ipfs/${cid}/${name}/`;
+  hideLoader(function() {
+    uploadCallback(cid, ipfsLink);
   });
 });
 QR_CODE.clear();
 
 // Generate decentralized QR code from folder
 $("#folderUpload").on("change", async function () {
-  $(".loader").show(900);
+  showLoader();
   var files = folderUpload.files;
   var cid = await client.put(files);
   let ipfsLink = `https://dweb.link/ipfs/${cid}/`;
-  // IPFS hash (CID)
-  document.getElementById("cid").textContent = cid;
-  // IPFS Infura link
-  document.getElementById("link").textContent = ipfsLink;
-  $("#svg-link").on("click", function () {
-    window.open(ipfsLink, "_blank").focus();
-  });
-  // Copy CID to clipboard when copy button is clicked
-  $("#svg-cid").on("click", function () {
-    navigator.clipboard.writeText(cid);
-  });
-  // Generate QR code
-  QR_CODE.makeCode(ipfsLink);
-  $(".loader").hide();
-  // Code to download qrcode
-  $("#svg-download").on("click", function () {
-    // gets the base64 source of the qr code image
-    var qrCodeSrc = document.getElementById("qr-code-image").src;
-    var a = document.createElement("a");
-    // an invisible a tag is given that href.
-    a.href = qrCodeSrc;
-    // filename for the qrcode is set
-    a.download = "dscan_QR.png";
-    document.body.appendChild(a);
-    // the a tag is clicked, triggering the download
-    a.click();
-    document.body.removeChild(a);
+  hideLoader(function() {
+    uploadCallback(cid, ipfsLink);
   });
 });
 QR_CODE.clear();
