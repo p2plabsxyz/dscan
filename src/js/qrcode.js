74378e31f518a31971d04351beb1bf5f2107dbc3
Akhilesh Thite <akhileshthite@Akhileshs-Mac-mini.local>
2022-03-08 20:21:42 +0530

    update the ipfs gateway to 'ipfs.infura.io' for faster loading

--- src/js/qrcode.js
+++ src/js/qrcode.js
@@ -34,31 +34,37 @@ function showLoader() {
 function uploadCallback(cid, ipfsLink) {
   // IPFS hash (CID)
   document.getElementById("cid").textContent = cid;
-  // IPFS Infura link
+  // IPFS gateway link
   document.getElementById("link").textContent = ipfsLink;
-  $("#svg-link").off().on("click", function () {
-    window.open(ipfsLink, "_blank").focus();
-  });
+  $("#svg-link")
+    .off()
+    .on("click", function () {
+      window.open(ipfsLink, "_blank").focus();
+    });
   // Copy CID to clipboard when copy button is clicked
-  $("#svg-cid").off().on("click", function () {
-    navigator.clipboard.writeText(cid);
-  });
+  $("#svg-cid")
+    .off()
+    .on("click", function () {
+      navigator.clipboard.writeText(cid);
+    });
   // Generate QR code
   QR_CODE.makeCode(ipfsLink);
   // Code to download qrcode
-  $("#svg-download").off().on("click", function () {
-    // gets the base64 source of the qr code image
-    var qrCodeSrc = document.querySelector("#qrcode img").src;
-    var a = document.createElement("a");
-    // an invisible a tag is given that href.
-    a.href = qrCodeSrc;
-    // filename for the qrcode is set
-    a.download = "dscan_QR.png";
-    document.body.appendChild(a);
-    // the a tag is clicked, triggering the download
-    a.click();
-    document.body.removeChild(a);
-  });
+  $("#svg-download")
+    .off()
+    .on("click", function () {
+      // gets the base64 source of the qr code image
+      var qrCodeSrc = document.querySelector("#qrcode img").src;
+      var a = document.createElement("a");
+      // an invisible a tag is given that href.
+      a.href = qrCodeSrc;
+      // filename for the qrcode is set
+      a.download = "dscan_QR.png";
+      document.body.appendChild(a);
+      // the a tag is clicked, triggering the download
+      a.click();
+      document.body.removeChild(a);
+    });
 }
 
 // Generate decentralized QR code from file
@@ -67,8 +73,8 @@ $("#fileUpload").on("change", async function () {
   var files = fileUpload.files;
   var name = files[0].name;
   var cid = await client.put(files);
-  let ipfsLink = `https://dweb.link/ipfs/${cid}/${name}/`;
-  hideLoader(function() {
+  let ipfsLink = `https://ipfs.infura.io/ipfs/${cid}/${name}/`;
+  hideLoader(function () {
     uploadCallback(cid, ipfsLink);
   });
 });
@@ -79,8 +85,8 @@ $("#folderUpload").on("change", async function () {
   showLoader();
   var files = folderUpload.files;
   var cid = await client.put(files);
-  let ipfsLink = `https://dweb.link/ipfs/${cid}/`;
-  hideLoader(function() {
+  let ipfsLink = `https://ipfs.infura.io/ipfs/${cid}/`;
+  hideLoader(function () {
     uploadCallback(cid, ipfsLink);
   });
 });
