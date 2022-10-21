9ef5869b7ff533886ebbc4df92e6cc22fc77d6c9
Luiz Paiva <luizgustavodematos@gmail.com>
2022-10-21 20:25:40 -0300

    fix: error when upload is cancelled

--- src/js/qrcode.js
+++ src/js/qrcode.js
@@ -164,8 +164,10 @@ function updateQRColor(colorPropName, hexCode = "#000000") {
 
 // Generate decentralized QR code from file
 $("#fileUpload").on("change", async function () {
-  showLoader();
   var files = fileUpload.files;
+  if (files.length === 0) return;
+
+  showLoader();
   var cid = await client.put(files, {
     onStoredChunk: getProgressUpdater(files),
     wrapWithDirectory: false,
@@ -179,8 +181,10 @@ QR_CODE_DISPLAY.clear();
 
 // Generate decentralized QR code from folder
 $("#folderUpload").on("change", async function () {
-  showLoader();
   var files = folderUpload.files;
+  if (files.length === 0) return;
+
+  showLoader();
   var cid = await client.put(files, {
     onStoredChunk: getProgressUpdater(files),
   });
